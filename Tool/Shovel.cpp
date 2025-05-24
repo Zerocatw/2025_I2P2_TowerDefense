5bffc80cf0050690b8409da029c3b14e22f91f76
Zer0catw <genie34s18g@gmail.com>
2025-05-25 07:53:01 +0800

    todo3-1(shovel)

---
+++ Tool/Shovel.cpp
@@ -0,0 +1,14 @@
+#include "Shovel.hpp"
+#include "Scene/PlayScene.hpp"
+#include "Turret/Turret.hpp"
+#include "Engine/GameEngine.hpp"
+
+PlayScene* Shovel::getPlayScene() {
+    return dynamic_cast<PlayScene*>(Engine::GameEngine::GetInstance().GetActiveScene());
+}
+
+Shovel::Shovel(float x, float y)
+    : Engine::Sprite("play/shovel.png", x, y){
+    Anchor.y += 8.0f / GetBitmapHeight();
+}
+
